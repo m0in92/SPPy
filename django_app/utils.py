e6ca2e70fe66febae56a8d2ee188df7ef1948b07
m0in92 <moinahmed100@gmail.com>
2023-11-30 16:19:49 -0500

    Extracted a json containing ECM parameters and started a js file

--- django_app/utils.py
+++ django_app/utils.py
@@ -14,11 +14,13 @@ import SPPy
 
 # BASEDIR_CSV = os.path.join('..', '..', 'SPPy', 'parameter_set')
 print(__file__)
-BASEDIR_CSV: str = os.path.join('..', 'parameter_sets')
-DICT_FILENAMES: dict = {'param_battery-cell.csv': 'bc',
-                        'param_electrolyte.csv': 'sep',
-                        'param_neg-electrode.csv': 'n',
-                        'param_pos-electrode.csv': 'p'}  # contains the filenames and their types
+BASEDIR_CSV_SP: str = os.path.join('..', 'parameter_sets')
+BASEDIR_CSV_ECM: str = os.path.join('..', 'parameter_sets_ecm')
+DICT_FILENAMES_ECM: dict = {'param.csv': 'bc'}
+DICT_FILENAMES_SP: dict = {'param_battery-cell.csv': 'bc',
+                           'param_electrolyte.csv': 'sep',
+                           'param_neg-electrode.csv': 'n',
+                           'param_pos-electrode.csv': 'p'}  # contains the filenames and their types
 
 
 def get_dict_params(filepath_to_csv: str, dict_params: dict, filetype: str) -> dict:
@@ -31,7 +33,8 @@ def get_dict_params(filepath_to_csv: str, dict_params: dict, filetype: str) -> d
         for i, row in enumerate(csv_reader):
             if i != 0:
                 if len(row[0].split(' [')) >= 2:
-                    param_name: str = row[0].split(' [')[0] + f"_{filetype} [" + row[0].split(' [')[-1]  # the string split
+                    param_name: str = row[0].split(' [')[0] + f"_{filetype} [" + row[0].split(' [')[
+                        -1]  # the string split
                     # is to add the filetype as parameter name_{file type} [units]
                 else:
                     param_name: str = row[0] + f"_{filetype}"
@@ -40,39 +43,57 @@ def get_dict_params(filepath_to_csv: str, dict_params: dict, filetype: str) -> d
     return dict_params
 
 
-def get_csv_filepath(parameter_name: str, csv_filename: str) -> str:
+def get_csv_filepath(parameter_name: str, csv_filename: str, param_type: str) -> str:
     """
     Returns the filepath to the csv file given the parameter name and csv file name.
     """
-    return os.path.join(BASEDIR_CSV, parameter_name, csv_filename)
+    if param_type == "ECM":
+        basedir: str = BASEDIR_CSV_ECM
+    elif param_type == "SP":
+        basedir: str = BASEDIR_CSV_SP
+    else:
+        raise ValueError(f'Invalid param_type: {param_type}. Available options are ECM, SP')
+    return os.path.join(basedir, parameter_name, csv_filename)
 
 
-def create_dict_parameterset() -> dict:
+def create_dict_parameterset(param_type: str) -> dict:
     """
     Returns a dict containing the information for all the parameter information. The key contains the parameterset name
     and the value is a dict containing the key/value as parameter name (in strings)/parameter value (in strings).
     """
     dict_parameter_set: dict = {}  # creates and empty dict for the json file.
     # iterate over all the filenames in the parameter sets directories
-    lst_parameterset_names: list = SPPy.battery_components.parameter_set_manager.ParameterSets.list_parameters_sets()
     dict_final: dict = {}
+    # dict_filename: dict = {}  # dict that intends to store the filenames containing parameters
+    if param_type == 'ECM':
+        dict_filename: dict = DICT_FILENAMES_ECM
+        lst_parameterset_names: list = SPPy.battery_components.parameter_set_manager.ECMParameterSets.lst_parameter_names()
+    elif param_type == 'SP':
+        dict_filename: dict = DICT_FILENAMES_SP
+        lst_parameterset_names: list = SPPy.battery_components.parameter_set_manager.ParameterSets.list_parameters_sets()
+    else:
+        raise ValueError(f"Invalid param_type {param_type}: valid options are 'ECM', 'SP'")
     for parameterset_name in lst_parameterset_names:
         # in the following, for each file create a dictionary containing parameter values
         dict_params: dict = {}  # intended to store all parameters for the a parameterset
-        for filename in DICT_FILENAMES.keys():  # for each above iteration iterate over all files
-            filepath: str = os.path.join(BASEDIR_CSV, parameterset_name, filename)
-            filetype: str = DICT_FILENAMES[filename]
+        for filename in dict_filename.keys():  # for each above iteration iterate over all files
+            filepath: str = get_csv_filepath(parameter_name=parameterset_name,
+                                             csv_filename=filename,
+                                             param_type=param_type)
+            filetype: str = dict_filename[filename]
             try:
                 param_dict = get_dict_params(filepath_to_csv=filepath, dict_params=dict_params, filetype=filetype)
             except FileNotFoundError as e:
                 print(e)
+        print(dict_final)
         dict_final[parameterset_name] = param_dict
     return dict_final
 
 
-def save_json_file(filepath_to_json: str) -> None:
+def save_json_file(filepath_to_json: str, param_type: str = 'SP') -> None:
     with open(filepath_to_json, 'w', encoding='utf-8') as json_file:
-        json_file.write(json.dumps(create_dict_parameterset(), indent=4))
+        json_file.write(json.dumps(create_dict_parameterset(param_type=param_type), indent=4))
 
 
-save_json_file('static/parameter_sets.json')
+# save_json_file('static/parameter_sets.json')
+save_json_file('static/parameter_sets_ecm.json', param_type='ECM')
