5463a6e68a4b1f5fe479cdb961656727ecaa2637
henry_hung <henryhung942000@gmail.com>
2023-12-05 12:29:24 -0500

    reactjs migration WIP

---
+++ django_app/static/src/pages/Sp_base_page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+export default function Sp_base_page() {
+  return (
+    <div className="Sp_base_jsx">
+      <Container_inputs/>
+      <Plots_outputs/>
+    </div>
+  );
+}
