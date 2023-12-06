0873e1b9c9b4a5ea310943cc3c7b0c6241b9aa67
henry_hung <henryhung942000@gmail.com>
2023-12-06 17:07:16 -0500

    Reactjs migration redo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HelloJuan</h1>
+      </header>
+      <body>
+        <div>
+          I HATE REACT!!1!1!! Zucc plz dont kill me
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
