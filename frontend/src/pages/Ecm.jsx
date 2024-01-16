8d1c841e5650f03d3909238a5651770e35909078
henry_hung <henryhung942000@gmail.com>
2024-01-16 12:53:26 -0500

    Reactjs migration redo WIP - Ecm frontend WIP 2

--- frontend/src/pages/Ecm.jsx
+++ frontend/src/pages/Ecm.jsx
@@ -9,9 +9,17 @@ function Ecm(){
     const [socLibInit, setSocLibInit] = useState(0.0);
     const [tempInit, setTempInit] = useState(298.0);
 
+    const [socValidity, setSocValidity] = useState(true)
+    const [tempValidity, setTempValidity] = useState(true)
+
     const [paramName, setParamName] = useState("test");
     const [cycler, setCycler] = useState("discharge");
     const [simParams, setSimParams] = useState({});
+
+    const [socLibSim, setSocLibSim] = useState([]);
+    const [tSim, setTSim] = useState([]);
+    const [tempSim, setTempSim] = useState([]);
+    const [vSim, setVSim] = useState([]);
 
 
      useEffect(() => {
@@ -38,10 +46,132 @@ function Ecm(){
       });
     }, [paramName]);
 
+    const baseVerifyFloatInput = (num) => {
+        if (isNaN(num)) {
+            return false;
+        } else {
+            // number must be non-negative at most 4 sigfigs
+            return num.match(/^\d*(\.\d{0,4})?$/);
+        }
+    }
+
+    const verifySOC = (e) => {
+        let nmbr = e.target.value.trim();
+        console.log(paramNameList)
+        if (baseVerifyFloatInput(nmbr) && Number(nmbr) <= 1.0) {
+            setSocValidity(true);
+            setSocLibInit(Number(nmbr));
+        } else {
+            setSocValidity(false);
+        }
+    }
+
+    const verifyTemp = (e) => {
+        let nmbr = e.target.value.trim();
+        setTempValidity(baseVerifyFloatInput(nmbr));
+        if (baseVerifyFloatInput(nmbr)) {
+            setTempInit(Number(nmbr))
+        }
+    }
+
+    const renderedParamNameList = paramNameList.map(item => <option key={item} value={item}>{item}</option>);
+    const renderedCyclerList = cyclerList.map(item => <option key={item} value={item}>{item}</option>);
+
+    function HandleSubmit(e) {
+        e.preventDefault();
+        if (socValidity && tempValidity) {
+            let formData = new FormData();
+            formData.append("parameter_name", paramName);
+            formData.append("cycler", cycler);
+            formData.append("soc_lib_init", socLibInit);
+            formData.append("temp_amb", tempInit);
+            axios.post(`${API_BASE_URL}/batterysim/ecm`, formData)
+                .then((rspn) => {
+                    console.log(rspn.data);
+                    setSocLibSim(JSON.parse(rspn.data["soc_lib"]));
+                    setTSim(JSON.parse(rspn.data["t_sim"]));
+                    setTempSim(JSON.parse(rspn.data["temp_sim"]));
+                    setVSim(JSON.parse(rspn.data["v_sim"]));
+                })
+                .catch((err) => {
+                console.error(err);
+            });
+        }
+    }
     return(
         <div>
             <div className="container">
+                <form method="post" onSubmit={HandleSubmit}>
+                    {/* see how forms are done: https://react.dev/reference/react-dom/components/select#reading-the-select-box-value-when-submitting-a-form
+                         */}
+                    <table>
+                    <tbody>
+                        <tr>
+                            <td>Parameter Name:</td>
+                            <td>
+                                <label>
+                                    <select value={paramName}
+                                        onChange={e => setParamName(e.target.value)}>
+                                        {renderedParamNameList}
+                                    </select>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Cycler:</td>
+                            <td>
+                                <label>
+                                    <select value={cycler}
+                                        onChange={e => setCycler(e.target.value)}>
+                                        {renderedCyclerList}
+                                    </select>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Initial LIB SOC:</td>
+                            <td>
+                                <label>
+                                    <input type="number"
+                                           min="0.0000"
+                                           max="1.0000"
+                                           step="0.0001"
+                                           defaultValue={0.0}
+                                           inputMode="numeric"
+                                           required={true}
+                                           onChange={e => verifySOC(e)}/>
+                                </label>
+                                {(!socValidity) && <span>Input must be a number between 0.0 and 1.0</span>}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Ambient Temperature [K]:</td>
+                            <td>
+                                <label>
+                                    <input type="number"
+                                           min="0.0000"
+                                           step="0.0001"
+                                           defaultValue={298.0}
+                                           inputMode="numeric"
+                                           required={true}
+                                           onChange={e => verifyTemp(e)}/>
+                                </label>
+                                {(!tempValidity) && <span>Input must be a number greater than 0.0</span>}
+                            </td>
+                        </tr>
+                    </tbody>
+                    </table>
+                    <button type="submit">Submit</button>
+                </form>
 
+            </div>
+            <div>
+
+            </div>
+            <div>
+                {RenderDataPoints({"Time [s]": tSim}, {"SOC LIB": socLibSim})}
+                {RenderDataPoints({"Time [s]": tSim}, {"Potential [V]": vSim})}
+                {RenderDataPoints({"Time [s]": tSim}, {"Temperature [C]": tempSim})}
             </div>
         </div>
     );
