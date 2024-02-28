2a3b07fd5f0e99c519c4453ed5da64f52ab13b9c
m0in92 <moinahmed100@gmail.com>
2024-02-27 19:31:17 -0500

    latest

--- examples/solvers/romsei.py
+++ examples/solvers/romsei.py
@@ -14,10 +14,6 @@ temp: float = 298.15
 cell = SPPy.BatteryCell.read_from_parametersets(parameter_set_name='test',
                                                 soc_init_p=SOC_init_p, soc_init_n=SOC_init_n,
                                                 temp_init=temp)
-cell.elec_n.i_s = 1.14264e-15  # in mol/m2/s
-soc_n: np.ndarray = np.linspace(0.01, 0.7568)
-ocp_n: np.ndarray = cell.elec_n.func_OCP(soc_n)
-
 solver: ROMSEISolver = ROMSEISolver(b_cell=cell)
 
 # print some class attributes and properties
@@ -34,16 +30,19 @@ print("kappa_SEI", solver.kappa)
 print(solver.solve_current(SOC_n=0.7522, OCP_n=cell.elec_n.func_OCP(0.7522),
                            temp=temp, I=1.656, rel_tol=1e-12))
 
-j_s: np.ndarray = np.array([solver.solve_current(SOC_n=soc_, OCP_n=ocp_,
+soc_n: np.ndarray = np.linspace(0.01, 0.7568)
+ocp_n: np.ndarray = cell.elec_n.func_OCP(soc_n)
+I_s: np.ndarray = np.array([solver.solve_current(SOC_n=soc_, OCP_n=ocp_,
                            temp=temp, I=1.656)[1] for soc_, ocp_ in zip(soc_n, ocp_n)])
-j_i: np.ndarray = np.array([solver.solve_current(SOC_n=soc_, OCP_n=ocp_,
+I_i: np.ndarray = np.array([solver.solve_current(SOC_n=soc_, OCP_n=ocp_,
                            temp=temp, I=1.656)[0] for soc_, ocp_ in zip(soc_n, ocp_n)])
+print(I_i)
 
 # plots
 fig, ax1 = plt.subplots()
-ax1.plot(soc_n, j_i, label="J_i")
+ax1.plot(soc_n, I_i, label="J_i")
 ax2 = ax1.twinx()
-ax2.plot(soc_n, j_s, color="red", label="J_s")
+ax2.plot(soc_n, I_s, color="red", label="J_s")
 
 plt.legend()
 plt.show()
