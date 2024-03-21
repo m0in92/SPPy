380830939901009201f29473a60956d0a504737b
m0in92 <moinahmed100@gmail.com>
2024-03-21 12:30:22 -0400

    Added examples for the electrolyte conc. however there seems to be a problem with the volume average solver

--- examples/solvers/electrolyte_conc_solvers/volume_average_c_e_solver.py
+++ examples/solvers/electrolyte_conc_solvers/volume_average_c_e_solver.py
@@ -58,4 +58,5 @@ print(conc_solver.conc_profile_p(x_p))
 
 plt.plot(x_n, conc_solver.conc_profile_n(L_value=x_n))
 plt.plot(x_p, conc_solver.conc_profile_p(L_value=x_p))
+plt.ticklabel_format(axis="x", scilimits=[-3, 1])
 plt.show()
