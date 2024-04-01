e4e36d6a97f66a20a38c5e95a675c946d96bbf18
m0in92 <moinahmed100@gmail.com>
2024-04-01 07:28:14 -0400

    minor refactoring pertaining to adding Python's type hinting

--- parameter_sets/test/funcs.py
+++ parameter_sets/test/funcs.py
@@ -9,19 +9,19 @@ def OCP_ref_p(SOC):
               - 0.05447 * np.arctan(18.99678 * SOC - 12.32362) - np.exp(78.24095 * SOC - 78.68074)
 
 
-def dOCPdT_p(SOC):
+def dOCPdT_p(SOC: float) -> float:
     num = -0.19952 + 0.92837*SOC - 1.36455 * SOC ** 2 + 0.61154 * SOC ** 3
     dem = 1 - 5.66148 * SOC + 11.47636 * SOC**2 - 9.82431 * SOC**3 + 3.04876 * SOC**4
     return (num/dem) * 1e-3 # since the original unit are of mV/K
 
 
-def OCP_ref_n(SOC):
+def OCP_ref_n(SOC: float) -> float:
     return 0.13966 + 0.68920 * np.exp(-49.20361 * SOC) + 0.41903 * np.exp(-254.40067 * SOC) \
             - np.exp(49.97886 * SOC - 43.37888) - 0.028221 * np.arctan(22.52300 * SOC - 3.65328) \
             -0.01308 * np.arctan(28.34801 * SOC - 13.43960)
 
 
-def dOCPdT_n(SOC):
+def dOCPdT_n(SOC: float) -> float:
     num = 0.00527 + 3.29927 * SOC - 91.79326 * SOC ** 2 + 1004.91101 * SOC ** 3 - \
           5812.27813 * SOC ** 4 + 19329.75490 * SOC ** 5 - 37147.89470 * SOC ** 6 + \
           38379.18127 * SOC ** 7 - 16515.05308 * SOC ** 8
